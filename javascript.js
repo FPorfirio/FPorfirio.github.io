429527fde284505d2a50b1bf20143d5bfc82af97
FPorfirio <facundojporfirio@gmail.com>
2018-04-10 16:21:46 -0300

    Testing js function

--- javascript.js
+++ javascript.js
@@ -1,23 +1,8 @@
-let buttons = document.getElementsByTagName("button");
-
-let aside = document.getElementById("aside");
-
-let about = document.getElementById("about");
-
-let door = document.getElementsByClassName("door");
+const form = document.forms;
 
 
 
-aside.addEventListener("click", function(e){
-    var btn = e.target.id;
-        for(var element of door){
-            if(element.classList.contains("closed")){
-                element.classList.remove("closed")
-            }
-            else{
-                element.classList.add("closed")
-            }
-        }   
+form[0].addEventListener('submit', function(e){
+    e.preventDefault();
 })
-
 
