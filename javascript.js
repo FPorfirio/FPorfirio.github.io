72faf524f2dd709c49e4852f64486b45f335df49
FPorfirio <facundojporfirio@gmail.com>
2018-05-05 01:13:42 -0300

    Dinamically nav function added

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 
+//Dom elements;
 const anchor = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])');
+const body = document.getElementById('body');
 const main = document.getElementById('main');
 const nav = document.getElementById("nav");
 const header = document.getElementsByTagName("header")[0];
 const aside = document.getElementById('aside');
-const body = document.getElementById('body');
-
-
+const grid = document.getElementById('grid');
 
 //Nav scroll function 
 function scrollTo(element, to, duration) {
@@ -23,19 +23,46 @@ function scrollTo(element, to, duration) {
 
 }
 
+
+//Nav links scroll handler
 let ele;
+let destination;
+
+let h2Height;
 
 for(let item of anchor) {
-    
     item.addEventListener('click', function(e){
         e.preventDefault();
         let hash = this.hash.slice(1);
+        h2Height = 0;
         ele = document.getElementById(hash);
-        let destination = main.offsetTop + ele.offsetTop;
+        destination = main.offsetTop + ele.offsetTop - h2Height;
         scrollTo(document.body, destination, 600)
     })
 }
 
+//Dynamic added nav function
+let windowsWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+function actualResizeHandler() {0
+    if(document.body.scrollTop >= nav.offsetTop && windowsWidth > 609 && nav.parentElement != aside) {
+        nav.id = ""
+        nav.className = "navM";
+        aside.appendChild(nav);
+    }   
+    else if(document.body.scrollTop >= nav.offsetTop && windowsWidth < 609 && nav.parentElement != main) {   
+        nav.id = "";
+        nav.className = "navS";
+        h2Height =  document.getElementsByTagName("h2")[1].clientHeight;
+        main.style.paddingTop = "0";
+        main.insertBefore(nav, main.firstChild)
+    }
+    else if (document.body.scrollTop <= 460) {
+        nav.id = "nav";
+        nav.className = "";
+        header.appendChild(nav);
+    }
+}
+  
 //Event limiter function(throttle)
 let resizeTimeout;
 function resizeThrottler() {
@@ -47,39 +74,5 @@ function resizeThrottler() {
     }
 }
 
-function actualResizeHandler() {0
-    if(document.body.scrollTop >= header.clientHeight) {
-        let removed = header.removeChild(nav);
-        removed.id = "nav"
-    }
-    
-    else {
-        header.appendChild(nav);
-    }
-
-}
-  
 window.addEventListener("scroll", resizeThrottler, false);
 
-
-/*
-
-for(let item of anchor) {
-
-
-
-item.addEventListener('click', function(e){
-    e.preventDefault();
-    var hash = this.hash.slice(1);
-    var element = document.getElementById(hash);
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-
-    })
-})
-
-}
-
- 
-*/
